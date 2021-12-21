a3aaaaff0018d138321a2c0ac02836e5f847e159
kohhM <88019822+kohhM@users.noreply.github.com>
2021-12-21 14:40:22 +0900

    Update 1207.py

--- 1207.py
+++ 1207.py
@@ -1,7 +1,7 @@
 from machine import Pin
 import xbee
 
-INPUT_PIN_ID = "D8"
+INPUT_PIN_ID = "D1"
 
 CO_addr = b'\x00\x13\xA2\x00\x41\xCB\xF8\xAC'
 
@@ -12,15 +12,25 @@ print(" +-------------------------+\n")
 xb = xbee.XBee()
 
 input_pin = Pin(INPUT_PIN_ID, Pin.IN, Pin.PULL_UP)
+sw_sleep = Pin("D2", Pin.IN, Pin.PULL_UP)
 
 NI = str(xbee.atcmd("NI"))
 
+def sw():
+    if sw_sleep.value() == 0:
+        return True
+    else:
+        return False
+
+
 while True:
-    sleep_ms = xb.sleep_now(True)
-    # sleep_ms = xb.sleep_now(None, True)
-    if xb.wake_reason() is xbee.PIN_WAKE:
-        try:
-            xbee.transmit(CO_addr, NI+"mdt")
-            print("sent mdt")
-        except Exception as e:
-            print("Transmit failure:", str(e))
+    if sw():
+        sleep_ms = xb.sleep_now(True)
+        # sleep_ms = xb.sleep_now(None, True)
+        if xb.wake_reason() is xbee.PIN_WAKE:
+            try:
+                xbee.transmit(CO_addr, NI+"mdt")
+                print("sent mdt")
+            except Exception as e:
+                print("Transmit failure:", str(e))
+                
