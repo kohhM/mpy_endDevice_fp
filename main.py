d9b71e9f513745a16365a87add89978fdae9d11a
kohhM <88019822+kohhM@users.noreply.github.com>
2022-02-14 18:37:02 +0900

    最終版

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-
 from machine import Pin
 import xbee
 
-INPUT_PIN_ID = "D1"
-
-CO_addr = b'\x00\x13\xA2\x00\x41\xCB\xF8\xAC'
-
 NI = str(xbee.atcmd("NI"))
-input_pin = Pin(INPUT_PIN_ID, Pin.IN, Pin.PULL_UP)
+input_pin = Pin("D1", Pin.IN, Pin.PULL_UP)
 
 xb = xbee.XBee()
 
@@ -16,7 +11,7 @@ while True:
         sleep_ms = xb.sleep_now(60000,True)
         if xb.wake_reason() is xbee.PIN_WAKE:
             try:
-                xbee.transmit(CO_addr, NI + "mdt")
+                xbee.transmit(xbee.ADDR_COORDINATOR, NI + "mdt")
             except Exception as e:
                 print("Transmit failure:", str(e))
                 
